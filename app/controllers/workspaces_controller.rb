112fac4412a1e9f7d9e39d3aab9adc891d41cda3
Ren-40424 <takashimaren0527@gmail.com>
2024-05-14 00:22:26 +0900

    ログインページへのリダイレクトを実装

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -1,4 +1,14 @@
 class WorkspacesController < ApplicationController
+  before_action :move_to_sign_in, only: :index
+
   def index
+  end
+
+  private
+
+  def move_to_sign_in
+    unless user_signed_in?
+      redirect_to user_session_path
+    end
   end
 end
