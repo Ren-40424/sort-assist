93e801a1f9b2445956e1582576d19477edf5056c
Ren-40424 <takashimaren0527@gmail.com>
2024-06-03 18:34:59 +0900

    いくつかの細かい修正

--- app/models/workspace_user.rb
+++ app/models/workspace_user.rb
@@ -1,11 +1,11 @@
 class WorkspaceUser < ApplicationRecord
-  validates :user,      presence: true
-  validates :workspace, presence: true
-  validates :role,      presence: true, numericality: { only_integer: true, less_than_or_equal_to: 3 }
+  validates :user_id,         presence: true
+  validates :workspace_id,    presence: true
+  validates :role_id,         presence: true, numericality: { only_integer: true, less_than_or_equal_to: 3 }
 
   belongs_to :user
   belongs_to :workspace
 
-  extend ActiveHash::Association::ActiveRecordExtensions
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :role
 end
