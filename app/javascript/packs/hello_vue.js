acfc8be6a344e0c76b8e8e217b6958053fd3167a
Ren-40424 <takashimaren0527@gmail.com>
2024-06-06 19:01:05 +0900

    不要な部分を削除

--- app/javascript/packs/hello_vue.js
+++
@@ -1,9 +0,0 @@
-/* eslint no-console: 0 */
-
-import { createApp } from 'vue'
-import App from '../app.vue'
-
-document.addEventListener('DOMContentLoaded', () => {
-  const app = createApp(App)
-  app.mount('#app')
-})
