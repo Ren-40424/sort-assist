e8ddfdd8491648456feb4202c8589e66599a6825
Ren-40424 <takashimaren0527@gmail.com>
2024-06-10 14:25:39 +0900

    検索時のクエリを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,9 @@ class UsersController < ApplicationController
 
   def search
     if params[:query].present?
-      already_added_users = WorkspaceUser.where(workspace_id: params[:workspace_id]).pluck(:user_id)
-      @users = User.where('name LIKE ?', "%#{params[:query]}%").where.not(id: already_added_users) # すでに追加済のユーザーは検索結果から省く
+      # すでに追加済のユーザーは検索結果から省く
+      already_added_user_ids = WorkspaceUser.where(workspace_id: params[:workspace_id]).select(:user_id)
+      @users = User.where('name LIKE ?', "%#{params[:query]}%").where.not(id: already_added_user_ids) 
     else
       @users = User.all
     end
