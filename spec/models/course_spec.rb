f790390aed374391a0564e5d997ca712057b0f71
Ren-40424 <takashimaren0527@gmail.com>
2024-06-24 22:09:32 +0900

    courseモデルの単体テスト

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @sheet = FactoryBot.create(:sheet)
+    @course = FactoryBot.build(:course, sheet: @sheet)
+  end
+
+  describe 'コース作成機能' do
+    context '作成できるとき' do
+      it 'すべての情報が適切なら登録できる' do
+        expect(@course).to be_valid
+      end
+
+      it '説明文がなくても登録できる' do
+        @course.explanation = ''
+        expect(@course).to be_valid
+      end
+    end
+
+    context '作成できないとき' do
+      it '名前が必須であること' do
+        @course.name = ''
+        @course.valid?
+        expect(@course.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it 'Sheetが紐づいていることが必須であること' do
+        @course.sheet = nil
+        @course.valid?
+        expect(@course.errors.full_messages).to include('Sheet must exist')
+      end
+    end
+  end
 end
