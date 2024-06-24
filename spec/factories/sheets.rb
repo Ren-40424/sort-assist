3d01f01f4774c1214235a5d34c2f720ace2ad886
Ren-40424 <takashimaren0527@gmail.com>
2024-06-24 21:59:31 +0900

    sheetモデルの単体テスト

--- spec/factories/sheets.rb
+++ spec/factories/sheets.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :sheet do
-    
+    name        { Faker::Team.name }
+    explanation { Faker::Lorem.sentence }
+
+    association :workspace
   end
 end
--- spec/models/sheet_spec.rb
+++ spec/models/sheet_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Sheet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @workspace = FactoryBot.create(:workspace)
+    @sheet = FactoryBot.build(:sheet, workspace: @workspace)
+  end
+
+  describe 'シート作成' do
+    context '作成できるとき' do
+      it '情報がすべて適切なら登録できる' do
+        expect(@sheet).to be_valid
+      end
+
+      it '説明文がなくても登録できる' do
+        @sheet.explanation = ''
+        expect(@sheet).to be_valid
+      end
+    end
+
+    context '作成できないとき' do
+      it '名前が必須であること' do
+        @sheet.name = ''
+        @sheet.valid?
+        expect(@sheet.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it 'Workspaceが紐づいていることが必須であること' do
+        @sheet.workspace = nil
+        @sheet.valid?
+        expect(@sheet.errors.full_messages).to include('Workspace must exist')
+      end
+    end
+  end
 end
