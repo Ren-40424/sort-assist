857ab9f6ba7f6cd9bbed108fcac59a0591428de9
Ren-40424 <takashimaren0527@gmail.com>
2024-06-27 23:34:34 +0900

    バリデーションの修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,7 @@
 class Address < ApplicationRecord
   validates :district, :address_from, :sheet_id, presence: true
-  validates :address_from, :address_to, format: { with: /^(?!-)(?!.*--)[\d-]+(?<!-)$/, message: '数値とハイフンのみを使用してください' }
+  validates :address_from, format: { with: /\A(?!-)(?!.*--)[\d-]+(?<!-)\z/, message: '数値とハイフンのみを使用してください' }
+  validates :address_to,   format: { with: /\A(?!-)(?!.*--)[\d-]+(?<!-)\z/, message: '数値とハイフンのみを使用してください' }, allow_blank: true
   validates :load_place, numericality: true
 
   belongs_to :sheet
